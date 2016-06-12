8ca8aa95bf380c89fc82f5bcaee89cb934c5f711
agnie <agnieszka.pula@gmail.com>
2016-06-12 20:44:53 +0200

    Fixed some null pointers

--- src/com/company/Graph.java
+++ src/com/company/Graph.java
@@ -1,21 +1,21 @@
 package com.company;
 
-import java.util.List;
+import java.util.ArrayList;
 
 /**
  * Created by agnie on 6/9/2016.
  */
 public class Graph<T> {
 
-    private List<Vertex> vertices;
-    private List<Edge> edges;
+    private ArrayList<Vertex> vertices;
+    private ArrayList<Edge> edges;
 
     public Graph() {
-        vertices = null;
-        edges = null;
+        vertices = new ArrayList<Vertex>();
+        edges = new ArrayList<Edge>();
     }
 
-    public Graph(List<Vertex> vertices) {
+    public Graph(ArrayList<Vertex> vertices) {
         this.vertices = vertices;
     }
 
@@ -24,41 +24,38 @@ public class Graph<T> {
     }
 
     public void addVertex(T data) {
-        if (!contains(data)){
+        if (!contains(data)) {
             Vertex newVertex = new Vertex(data);
             vertices.add(newVertex);
-        }
-        else {
+        } else {
             System.out.println("The graph already contains such vertex");
         }
     }
 
     public void removeVertex(int index) {
-        if (index > -1 && index < vertices.size()){
+        if (index > -1 && index < vertices.size()) {
             Vertex vertex = vertices.get(index);
             vertices.remove(index);
             //mopping up the orphaned edges
             removeEdges(vertex);
-        }
-        else {
+        } else {
             System.out.println("Invalid index");
         }
     }
 
     public void removeVertex(T data) {
         Vertex vertex = search(data);
-        if (vertex!=null){
+        if (vertex != null) {
             vertices.remove(vertex);
             //mopping up the orphaned edges
             removeEdges(vertex);
-        }
-        else {
+        } else {
             System.out.println("Such element doesn't exist in the graph");
         }
     }
 
     public void addEdge(int start, int end, int weight) {
-        if (isValidEdge(start,end)) {
+        if (isValidEdge(start, end)) {
             Vertex startVertex = vertices.get(start);
             Vertex endVertex = vertices.get(end);
             addEdge(startVertex, endVertex, weight);
@@ -80,18 +77,17 @@ public class Graph<T> {
         addEdge(start, end, 1);
     }
 
-    public void removeEdge(Vertex start, Vertex end){
-        Edge edge = search(start,end);
-        if (edge!=null){
+    public void removeEdge(Vertex start, Vertex end) {
+        Edge edge = search(start, end);
+        if (edge != null) {
             edges.remove(edge);
-        }
-        else {
+        } else {
             System.out.println("No such edges");
         }
     }
 
-    public void removeEdge(int start, int end){
-        if (isValidEdge(start,end)){
+    public void removeEdge(int start, int end) {
+        if (isValidEdge(start, end)) {
             Vertex startVertex = vertices.get(start);
             Vertex endVertex = vertices.get(end);
             removeEdge(start, end);
@@ -100,66 +96,82 @@ public class Graph<T> {
         }
     }
 
-    private void removeEdges(Vertex vertex){
-        for (Edge edge : edges){
-            if (edge.contains(vertex)){
+    private void removeEdges(Vertex vertex) {
+        for (Edge edge : edges) {
+            if (edge.contains(vertex)) {
                 edges.remove(edge);
             }
         }
     }
 
     public void printEdges() {
-        for (Edge edge : edges) {
-            System.out.println(edge);
+        if (edges != null) {
+            for (Edge edge : edges) {
+                System.out.println(edge);
+            }
+        } else {
+            System.out.println("Graph has no edges");
         }
     }
 
     public void printVertices() {
-        for (Vertex vertex : vertices) {
-            System.out.println(vertex);
+        if (vertices != null) {
+            for (Vertex vertex : vertices) {
+                System.out.println(vertex);
+            }
+        } else {
+            System.out.println("Graph is empty");
         }
     }
 
     public boolean contains(T data) {
-        for (Vertex vertex : vertices) {
-            if (vertex.getData() == data) {
-                return true;
+        if (vertices != null) {
+            for (Vertex vertex : vertices) {
+                if (vertex.getData() == data) {
+                    return true;
+                }
             }
         }
         return false;
     }
 
-    private boolean contains(Vertex start, Vertex end){
-        for (Edge edge : edges){
-            if (edge.contains(start) && edge.contains(end)){
-                return true;
+    private boolean contains(Vertex start, Vertex end) {
+        if (edges != null) {
+            for (Edge edge : edges) {
+                if (edge.contains(start) && edge.contains(end)) {
+                    return true;
+                }
             }
         }
         return false;
     }
 
-    private Vertex search(T data){
-        for (Vertex vertex : vertices) {
-            if (vertex.getData() == data) {
-                return vertex;
+    private Vertex search(T data) {
+        if (vertices != null) {
+            for (Vertex vertex : vertices) {
+                if (vertex.getData() == data) {
+                    return vertex;
+                }
             }
         }
         return null;
     }
 
-    private Edge search(Vertex start, Vertex end){
-        for (Edge edge : edges){
-            if (edge.contains(start) && edge.contains(end)){
-                return edge;
+    private Edge search(Vertex start, Vertex end) {
+        if (edges != null) {
+            for (Edge edge : edges) {
+                if (edge.contains(start) && edge.contains(end)) {
+                    return edge;
+                }
             }
         }
         return null;
     }
 
-    private boolean isValidEdge(int start, int end){
-        if (start>-1 && start<edges.size()){
-            if (end>-1 && end<edges.size()){
-                if (end!=start){
+    private boolean isValidEdge(int start, int end) {
+        if (start > -1 && start < edges.size()) {
+            if (end > -1 && end < edges.size()) {
+                if (end != start) {
                     return true;
                 }
             }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,14 @@ package com.company;
 
 public class Main {
 
+    public static void presentGraph() {
+        Graph<Integer> graph = new Graph<Integer>();
+        graph.addVertex(3);
+        graph.addVertex(5);
+        graph.printVertices();
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        presentGraph();
     }
 }
