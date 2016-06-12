3394dcf571afa5a8bfbe846ba60e3cc741cf855c
agnie <agnieszka.pula@gmail.com>
2016-06-12 20:06:05 +0200

    Added removal of edges

--- src/com/company/Graph.java
+++ src/com/company/Graph.java
@@ -35,7 +35,10 @@ public class Graph<T> {
 
     public void removeVertex(int index) {
         if (index > -1 && index < vertices.size()){
+            Vertex vertex = vertices.get(index);
             vertices.remove(index);
+            //mopping up the orphaned edges
+            removeEdges(vertex);
         }
         else {
             System.out.println("Invalid index");
@@ -46,6 +49,8 @@ public class Graph<T> {
         Vertex vertex = search(data);
         if (vertex!=null){
             vertices.remove(vertex);
+            //mopping up the orphaned edges
+            removeEdges(vertex);
         }
         else {
             System.out.println("Such element doesn't exist in the graph");
@@ -53,7 +58,7 @@ public class Graph<T> {
     }
 
     public void addEdge(int start, int end, int weight) {
-        if (start > -1 && start < vertices.size() && end > start && end < vertices.size()) {
+        if (isValidEdge(start,end)) {
             Vertex startVertex = vertices.get(start);
             Vertex endVertex = vertices.get(end);
             addEdge(startVertex, endVertex, weight);
@@ -73,6 +78,34 @@ public class Graph<T> {
 
     public void addEdge(Vertex start, Vertex end) {
         addEdge(start, end, 1);
+    }
+
+    public void removeEdge(Vertex start, Vertex end){
+        Edge edge = search(start,end);
+        if (edge!=null){
+            edges.remove(edge);
+        }
+        else {
+            System.out.println("No such edges");
+        }
+    }
+
+    public void removeEdge(int start, int end){
+        if (isValidEdge(start,end)){
+            Vertex startVertex = vertices.get(start);
+            Vertex endVertex = vertices.get(end);
+            removeEdge(start, end);
+        } else {
+            System.out.println("Invalid data");
+        }
+    }
+
+    private void removeEdges(Vertex vertex){
+        for (Edge edge : edges){
+            if (edge.contains(vertex)){
+                edges.remove(edge);
+            }
+        }
     }
 
     public void printEdges() {
@@ -96,6 +129,15 @@ public class Graph<T> {
         return false;
     }
 
+    private boolean contains(Vertex start, Vertex end){
+        for (Edge edge : edges){
+            if (edge.contains(start) && edge.contains(end)){
+                return true;
+            }
+        }
+        return false;
+    }
+
     private Vertex search(T data){
         for (Vertex vertex : vertices) {
             if (vertex.getData() == data) {
@@ -105,5 +147,24 @@ public class Graph<T> {
         return null;
     }
 
+    private Edge search(Vertex start, Vertex end){
+        for (Edge edge : edges){
+            if (edge.contains(start) && edge.contains(end)){
+                return edge;
+            }
+        }
+        return null;
+    }
+
+    private boolean isValidEdge(int start, int end){
+        if (start>-1 && start<edges.size()){
+            if (end>-1 && end<edges.size()){
+                if (end!=start){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
 }
